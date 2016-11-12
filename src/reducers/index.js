fef8c31b29299105ca8dd5f6d60469c327f821bb
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2016-11-11 17:56:05 -0800

    Slight reducer fixes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,1 @@
-export * from './user';  
+export { default as user } from './user';
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,7 +1,7 @@
 import Immutable from 'immutable';
 
-const User = Immutable.Record({ email: null }); 
+const User = Immutable.Record({ email: null });
 
 export default function user(prevState = new User(), action) {
-  return new User(action.payload.user); 
+  return new User();
 }
