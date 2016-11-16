85835b17175a45f8c6210bb15f7bd6ef001aeefc
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2016-11-15 17:29:29 -0800

    Refactor for readability

--- src/components/LanguageDisplay.js
+++ src/components/LanguageDisplay.js
@@ -1,6 +1,11 @@
 import * as d3 from 'd3';
 import React, { Component } from 'react';
 
+const REMOVE_DURATION = (oldSkills, newSkills) => 300;
+const REMOVE_DELAY = (oldSkills, newSkills) => 0;
+const MOVE_DURATION = (oldSkills, newSkills) => 300;
+const MOVE_DELAY = (oldSkills, newSkills) => oldSkills.length > newSkills.length ? 300 : 0;
+
 class D3LanguageDisplay {
   constructor(parentElement, language, select, selected, options) {
     this.width = options ? options.width || 600 : 600;
@@ -16,69 +21,56 @@ class D3LanguageDisplay {
   }
   update(language, selected) {
     let skills = language.childSkills.sort((a, b) => b.impact - a.impact).toArray();
+    let oldSkills = this.svg.selectAll('.impact').data();
+   
     let impactLines = this.svg.selectAll('.impact').data(skills);
-    
-    impactLines.exit().transition().duration(300)
-      .attr('x1', 0)
-      .attr('x2', 0)
-      .remove();
-    impactLines = impactLines.enter().append('line')
-      .attr('stroke-width', 0)
-      .attr('x1', 0)
-      .attr('x2', 0)
-      .attr('y1', this.height)
-      .attr('y2', this.height)
-      .merge(impactLines);
-    
-    impactLines.attr('class', 'impact')
-      .attr('data-skill', skill => skill.name)
-      .attr('data-selected', skill => skill == selected);
+    let rankingLines = this.svg.selectAll('.ranking').data(skills);
+    let hiddenLines = this.svg.selectAll('.hidden').data(skills);
 
-    impactLines.transition().delay(300).duration(300)
+    // remove lines if we have too many 
+    impactLines.exit().transition().duration(REMOVE_DURATION).delay(REMOVE_DELAY).attr('x1', 0).attr('x2', 0).remove();
+    rankingLines.exit().transition().duration(REMOVE_DURATION).delay(REMOVE_DELAY).attr('x1', 0).attr('x2', 0).remove();
+    hiddenLines.exit().transition().duration(REMOVE_DURATION).delay(REMOVE_DELAY).attr('x1', 0).attr('x2', 0).remove();
+    
+    // add lines if we have too few 
+    impactLines = impactLines.enter().append('line')
+      .attr('stroke-width', 0).attr('x1', 0).attr('x2', 0).attr('y1', this.height).attr('y2', this.height)
+      .merge(impactLines);
+    rankingLines = rankingLines.enter().append('line')
+      .attr('stroke-width', 0).attr('x1', 0).attr('x2', 0).attr('y1', this.height).attr('y2', this.height)
+      .merge(rankingLines);
+    hiddenLines = hiddenLines.enter().append('line')
+      .attr('stroke-width', 0).attr('x1', 0).attr('x2', 0).attr('y1', this.height).attr('y2', this.height)
+      .merge(hiddenLines);
+    
+    // add proper classes and names
+    impactLines.attr('class', 'impact').attr('data-skill', skill => skill.name).attr('data-selected', skill => skill == selected);
+    rankingLines.attr('class', 'ranking').attr('data-skill', skill => skill.name).attr('data-selected', skill => skill == selected);
+    hiddenLines.attr('class', 'hidden').attr('data-skill', skill => skill.name);
+
+    // transition to new position
+    impactLines.transition().delay(MOVE_DELAY(oldSkills, skills)).duration(MOVE_DURATION)
       .attr('x1', 0)
       .attr('x2', skill => this.width * skill.impact / skills[0].impact)
       .attr('y1', (skill, index) => this.height * (index + 1) / skills.length)
       .attr('y2', (skill, index) => this.height * (index + 1) / skills.length)
       .attr('stroke-width', -8 + this.height / skills.length);
 
-    let rankingLines = this.svg.selectAll('.ranking').data(skills);
-    rankingLines.exit().transition().duration(300)
-      .attr('x1', 0)
-      .attr('x2', 0)
-      .remove();
-    rankingLines = rankingLines.enter().append('line')
-      .attr('stroke-width', 0)
-      .attr('x1', 0)
-      .attr('x2', 0)
-      .attr('y1', this.height)
-      .attr('y2', this.height)
-      .merge(rankingLines);
-    
-    rankingLines
-      .attr('class', 'ranking')
-      .attr('data-skill', skill => skill.name)
-      .attr('data-selected', skill => skill == selected);
-
-    
-    rankingLines.transition().delay(300).duration(300)
+    rankingLines.transition().delay(MOVE_DELAY(oldSkills, skills)).duration(MOVE_DURATION)
       .attr('x1', 0)
       .attr('x2', skill => this.width * skill.rank * skill.impact / skills[0].impact)
       .attr('y1', (skill, index) => this.height * (index + 1) / skills.length)
       .attr('y2', (skill, index) => this.height * (index + 1) / skills.length)
       .attr('stroke-width', -8 + this.height / skills.length);
     
-    let hiddenLines = this.svg.selectAll('.hidden').data(skills);
-    hiddenLines.exit().remove();
-    hiddenLines = hiddenLines.enter().append('line').merge(hiddenLines);
-    hiddenLines.attr('class', 'hidden').attr('data-skill', skill => skill.name)
-
-    hiddenLines.transition().delay(300).duration(300)
+    hiddenLines.transition().delay(MOVE_DELAY(oldSkills, skills)).duration(MOVE_DURATION)
       .attr('x1', 0)
       .attr('x2', this.width)
       .attr('y1', (skill, index) => this.height * (index + 1) / skills.length)
       .attr('y2', (skill, index) => this.height * (index + 1) / skills.length)
       .attr('stroke-width', this.height / skills.length)
-    
+
+    // add mouseover handler 
     hiddenLines.on('mouseover', this.select);
   }
 }
